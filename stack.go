478af4f39fd89c68c0d0dab5c888ce8d2cd7af00
Guo, Jingfeng <jingfeng.guo@intel.com>
2020-10-09 14:18:11 +0800

    first commit

---
+++ stack.go
@@ -0,0 +1,40 @@
+package stack
+
+import (
+	"github.com/cheekybits/genny/generic"
+	"sync"
+)
+
+type Item generic.Type
+
+type ItemStack struct {
+	items []Item
+	lock  sync.RWMutex
+}
+
+func (s *ItemStack) New() *ItemStack {
+	s.items = []Item{}
+	return s
+}
+
+func (s *ItemStack) Push(t Item) {
+	s.lock.Lock()
+	s.items = append(s.items, t)
+	s.lock.Unlock()
+}
+
+func (s *ItemStack) Pop() *Item {
+	s.lock.Lock()
+	item := s.items[len(s.items)-1]
+	s.items = s.items[:len(s.items)-1 ]
+	s.lock.Unlock()
+	return &item
+}
+
+func (s *ItemStack) IsEmpty() bool {
+	return len(s.items) == 0
+}
+
+func (s *ItemStack) Size() int {
+	return len(s.items)
+}
